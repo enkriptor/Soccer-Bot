267825314f5958592e0f99c24caca6e3bc5b1253
silloInTheBarn <silloInTheBarn@protonmail.com>
2020-06-25 20:27:06 +0530

    commits added

--- soccerBotSinglePlayer.py
+++ soccerBotSinglePlayer.py
@@ -1,24 +1,32 @@
 import discord, random
 
+#variable initiation
 client = discord.Client()
 sieve = []
 humanScore, botScore, botChance, humanChance, chance, matchStarter, matchDecider = 0, 0, False, True, 0, False, ["heads", "tails"]
 
+#bot login and startup
 @client.event
 async def on_ready():
 	print("We logged in as {0.user}".format(client))
 
+#bot event block
 @client.event
 async def on_message(message):
+	#globalizing variables
 	global humanScore, botScore, botChance, humanChance, chance, matchStarter, matchDecider, sieve
+	#failsafe run
 	if(message.author == client.user):
 		return
-
+	#real game code block
+	#rule block
 	if(message.content == '!soccerrules'):
 		await message.channel.send('Simple rules: \n type \'!soccerplay\' to start the play! \n type 0 for left-side kick\n type 1 for center kick\n type 2 for right-side kick')
+	#play block
 	if(message.content == '!soccerplay'):
 		await message.channel.send('Let\'s start the play!')
 		await message.channel.send('Type heads or tails for toss!')
+	#toss block
 	if(message.content in matchDecider):
 		if(message.content == matchDecider[random.randint(0,1)]):
 			matchStarter = True
@@ -30,9 +38,13 @@ async def on_message(message):
 			botChance = True
 			humanChance = False
 			await message.channel.send("match started! bot won the toss")
+	#game starter block
 	if(matchStarter):
+		#chance decided
 		if(not botChance and humanChance):
+			#matching with kicks
 			if(message.content=="1" or message.content=="2" or message.content=="0"):
+				#bot answering random reaction
 				botAnswer = str(random.randint(0,2))
 				sieve.append(botAnswer)
 				if(botAnswer == message.content):
@@ -40,6 +52,7 @@ async def on_message(message):
 				else:
 					humanScore += 1
 					await message.channel.send(botAnswer+' What a goal!')
+			#end round after 5 chances
 			if(len(sieve) == 5):
 				chance += 1
 				await message.channel.send('Well played! Your score '+str(humanScore))
@@ -66,7 +79,9 @@ async def on_message(message):
 					sieve = []
 					botChance = False
 					humanChance = True
+		#end of game
 		if(chance == 2):
+			#score evaluation
 			if(humanScore > botScore):
 				await message.channel.send("You win!")
 			elif(humanScore == botScore):
@@ -75,4 +90,5 @@ async def on_message(message):
 				await message.channel.send("You lose!")
 			matchStarter = False
 
+#run the discord token
 client.run('DISCORD_TOKEN')
